cb2c776908b8457e2e2d4e4602928f835a0c5c2c
CBadez10 <Cam_Badenoch10@live.com>
2021-05-23 11:21:17 -0500

    Text Change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Docker Reload
+          Docker Test App
         </p>
         <a
           className="App-link"
